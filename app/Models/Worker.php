8d3894f3f91f8529f06718d6b87e4ef07b4173f7
Zhanibek <janibeksultanov97@gmail.com>
2024-11-04 16:24:17 +0500

    Create Worker model with migration

---
+++ app/Models/Worker.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Worker extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'workers';
+    protected $guarded = false;
+}
